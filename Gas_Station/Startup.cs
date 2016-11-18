6858e81556124eff63df9007766504335bd42209
dimoskag <dimoskagiaras@gmail.com>
2016-11-18 17:11:44 +0200

    New Startup

--- Gas_Station/Startup.cs
+++ Gas_Station/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Gas_Station.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Gas_Station.Startup))]
@@ -9,6 +12,42 @@ namespace Gas_Station
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        //Default User and Admin Roles for login
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //Create Admin Role
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            //Create DefaultUser Role
+            if (!roleManager.RoleExists("DefaultUser"))
+            {
+                var role = new IdentityRole();
+                role.Name = "DefaultUser";
+                roleManager.Create(role);
+            }
+
+            //Create an Admin
+            //var user = new ApplicationUser();
+            //user.UserName = "test@gmail.com";
+            //user.Email = "test@gmail.com";
+            //string Password = "123456";
+            //var adminUser1 = UserManager.Create(user,Password);
+            //if (adminUser1.Succeeded)
+            //{
+            //    var assignRole = UserManager.AddToRole(user.Id, "Admin");
+            //}
         }
     }
 }
