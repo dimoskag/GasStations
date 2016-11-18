99d65f3899ebd82c82ad920895afcef5512b32d0
dimoskag <dimoskagiaras@gmail.com>
2016-11-18 14:37:24 +0200

    removed adress from RegisterViewModel

--- Gas_Station/Models/AccountViewModels.cs
+++ Gas_Station/Models/AccountViewModels.cs
@@ -70,10 +70,6 @@ namespace Gas_Station.Models
         public string Email { get; set; }
 
         [Required]
-        [Display(Name ="Address")]
-        public string Address { get; set; }
-
-        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
